1979b9c0608a28307291aab83add076d210b40bd
TuringMac <turingmac@hotmail.com>
2017-11-29 22:51:16 -0500

    SGD, test accuracy = 0.681

--- A3/q1.py
+++ A3/q1.py
@@ -8,9 +8,10 @@ import sklearn
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
 
 def load_data():
     # import and filter data
@@ -61,7 +62,7 @@ def logistic(tfidf_train, train_labels, tfidf_test, test_labels):
     tfidf_test = (tfidf_test > 0).astype(int)
     tfidf_train, tfidf_validation, train_labels, validation_labels = train_test_split(tfidf_train, train_labels, test_size = 0.2)
 
-    model = LogisticRegression(C = 1.0, penalty = 'l1', tol = 1e-6)
+    model = LogisticRegression(C = 1.0, penalty = 'l1', tol = 1e-4)
     Cs = [1, 5, 10, 20, 30, 40, 50]
     train_accuracy = []
     valid_accuracy = []
@@ -73,20 +74,36 @@ def logistic(tfidf_train, train_labels, tfidf_test, test_labels):
         validation_pred = model.predict(tfidf_validation)
         valid_accuracy.append((validation_pred == validation_labels).mean())
 
-    opt_C = Cs[int(np.argmax(valid_accuracy))]
-    print('Optimal C for logistic regression = {}'.format(opt_C))
-    print('Logistic regression train accuracy = {}'.format(train_accuracy[opt_C]))
-    print('Logistic regression validation accuracy = {}'.format(valid_accuracy[opt_C]))
+    opt_C_index = int(np.argmax(valid_accuracy))
+    print('Optimal C for logistic regression = {}'.format(Cs[opt_C_index]))
+    print('Logistic regression train accuracy = {}'.format(train_accuracy[opt_C_index]))
+    print('Logistic regression validation accuracy = {}'.format(valid_accuracy[opt_C_index]))
     test_pred = model.predict(tfidf_test)
     print('Logistic regression test accuracy = {}'.format((test_pred == test_labels).mean()))
+
+    return model
+
+
+def SGD(tfidf_train, train_labels, tfidf_test, test_labels):
+    # training the stochastic gradient descent model
+    model = SGDClassifier(alpha = 0.01, tol = 1e-4)
+    model.fit(tfidf_train, train_labels)
+
+    train_pred = model.predict(tfidf_train)
+    print('SGD train accuracy = {}'.format((train_pred == train_labels).mean()))
+    test_pred = model.predict(tfidf_test)
+    print('SGD test accuracy = {}'.format((test_pred == test_labels).mean()))
+
+    return model
 
 if __name__ == '__main__':
     train_data, test_data = load_data()
     train_bow, test_bow, feature_names = bow_features(train_data, test_data)
     train_tfidf, test_tfidf, feature_names = tf_idf_features(train_data, test_data)
 
-    print('BernoulliNB baseline')
+    print('### BernoulliNB baseline ###')
     bnb_model = bnb_baseline(train_tfidf, train_data.target, test_tfidf, test_data.target)
-    print('Logistic regression')
+    print('### Logistic regression ###')
     logistic_model = logistic(train_tfidf, train_data.target, test_tfidf, test_data.target)
-    
+    print('### Stochastic gradient descent ###')
+    SGD_model = SGD(train_tfidf, train_data.target, test_tfidf, test_data.target)
