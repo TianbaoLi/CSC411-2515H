216f4c13cb990a1b54ed69c8f53bb8263811596e
TuringMac <turingmac@hotmail.com>
2017-12-01 00:34:02 -0500

    Opt SVM, test accuracy = 0.678

--- A3/q1.py
+++ A3/q1.py
@@ -135,24 +135,23 @@ def SGD(tfidf_train, train_labels, tfidf_test, test_labels):
 
 def SVM(tfidf_train, train_labels, tfidf_test, test_labels):
     # training the support vector machine model
-    tfidf_train, tfidf_validation, train_labels, validation_labels = train_test_split(tfidf_train, train_labels, test_size = 0.2)
-
-    model = svm.LinearSVC(penalty = "l1", dual = False, tol = 1e-3)
-    Cs = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5]
-    train_accuracy = []
-    valid_accuracy = []
+    splits = 5
+    kf = KFold(splits, shuffle = True, random_state = 0)
+    Cs = [0.01, 0.1, 1, 5]
+    scores = []
     for c in Cs:
-        model.set_params(C = c)
-        model.fit(tfidf_train, train_labels)
-        train_pred = model.predict(tfidf_train)
-        train_accuracy.append((train_pred == train_labels).mean())
-        validation_pred = model.predict(tfidf_validation)
-        valid_accuracy.append((validation_pred == validation_labels).mean())
+        model = svm.LinearSVC(penalty = "l1", dual = False, tol = 1e-3, C = c)
+        score = cross_val_score(model, tfidf_train, train_labels, cv = kf)
+        scores.append(np.mean(score))
 
-    opt_C_index = int(np.argmax(valid_accuracy))
-    print('Optimal C for logistic regression = {}'.format(Cs[opt_C_index]))
-    print('SVM train accuracy = {}'.format(train_accuracy[opt_C_index]))
-    print('SVM validation accuracy = {}'.format(valid_accuracy[opt_C_index]))
+    opt_C_index = int(np.argmax(scores))
+    opt_C = Cs[opt_C_index]
+
+    print('Optimal C for support vector machine = {}'.format(opt_C))
+    model = LogisticRegression(C=opt_C)
+    model.fit(tfidf_train, train_labels)
+    train_pred = model.predict(tfidf_train)
+    print('SVM train accuracy = {}'.format((train_pred == train_labels).mean()))
     test_pred = model.predict(tfidf_test)
     print('SVM test accuracy = {}'.format((test_pred == test_labels).mean()))
 
@@ -231,10 +230,10 @@ if __name__ == '__main__':
     #mnb_model = mnb(train_tfidf, train_data.target, test_tfidf, test_data.target)
     #print('### Logistic regression ###')
     #logistic_model = logistic(train_tfidf, train_data.target, test_tfidf, test_data.target)
-    print('### Stochastic gradient descent ###')
-    SGD_model = SGD(train_tfidf, train_data.target, test_tfidf, test_data.target)
-    #print('### Support vector machine ###')
-    #SVM_model = SVM(train_tfidf, train_data.target, test_tfidf, test_data.target)
+    #print('### Stochastic gradient descent ###')
+    #SGD_model = SGD(train_tfidf, train_data.target, test_tfidf, test_data.target)
+    print('### Support vector machine ###')
+    SVM_model = SVM(train_tfidf, train_data.target, test_tfidf, test_data.target)
     #print('### K nearest neighbors ###')
     #KNN_model = KNN(train_tfidf, train_data.target, test_tfidf, test_data.target)
     #print('### Decision tree ###')
